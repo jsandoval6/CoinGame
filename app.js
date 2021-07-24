6953a200f963383c5b4c21657c72c6a5865c250e
David Sandoval <davidsandoval281994@gmail.com>
2021-07-24 11:53:20 -0600

    first commit

--- app.js
+++ app.js
@@ -9,3 +9,50 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin');
+
+window.addEventListener('keydown', function (e) {
+	const currTop = extractPosition(avatar.style.top);
+	const currLeft = extractPosition(avatar.style.left);
+	if ((e.key === 'ArrowDown' || e.key === 'Down') && currTop + 50 < window.innerHeight - avatar.getBoundingClientRect().height) {
+		moveVertical(avatar, 50, currTop);
+	}
+	else if ((e.key === "ArrowUp" || e.key === 'Up') && currTop - 50 > 0) {
+		moveVertical(avatar, -50, currTop);
+	}
+	else if ((e.key === "ArrowRight" || e.key === 'Right') && currLeft + 50 < (window.innerWidth - avatar.getBoundingClientRect().width)) {
+		moveHorizontal(avatar, 50, currLeft)
+		avatar.style.transform = 'scale(1,1)';
+	}
+	else if ((e.key === "ArrowLeft" || e.key === 'Left') && currLeft - 50 > 0) {
+		moveHorizontal(avatar, -50, currLeft)
+		avatar.style.transform = 'scale(-1,1)';
+	}
+	if (isTouching(avatar, coin)) {
+		moveCoin();
+	}
+});
+
+const moveVertical = (element, amount, top) => {
+	element.style.top = `${top + amount}px`;
+}
+
+const moveHorizontal = (element, amount, left) => {
+	element.style.left = `${left + amount}px`;
+}
+
+const extractPosition = (pos) => {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0, -2));
+}
+
+const moveCoin = () => {
+	const y = ~~(Math.random() * (window.innerHeight - coin.getBoundingClientRect().width));
+	const x = ~~(Math.random() * (window.innerWidth - coin.getBoundingClientRect().height));
+	coin.style.top = `${y}px`;
+	coin.style.left = `${x}px`;
+}
+
+moveCoin();
